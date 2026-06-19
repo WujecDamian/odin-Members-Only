6704c836367a3927dd783fccaa1c06b2d593518d
Damian Wujec <wujecdamian12@gmail.com>
2026-06-19 20:12:11 +0200

    Fix user creation logic by capturing username and password fields

--- Controllers/signUpController.js
+++ Controllers/signUpController.js
@@ -6,6 +6,9 @@ const renderForm = (req, res) => {
 
 const createUser = async (req, res) => {
   const fullname = req.body.fullName;
+  const username = req.body.username;
+  const password = req.body.password;
+  const passwordConfirm = req.body.password;
   const user = await db.createUser();
   res.redirect("/");
 };
