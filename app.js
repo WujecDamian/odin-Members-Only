f6a34408c03c5c655598d8a9e0d49147997713ba
Damian Wujec <wujecdamian12@gmail.com>
2026-06-19 21:25:02 +0200

    feat: add passport configuration & functions

--- app.js
+++ app.js
@@ -2,18 +2,67 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
-
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("./Models/queries");
 //Ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+//Passport
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+//* setting up the LocalStrategy
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByUsername(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 //Routes
 const signUpRouter = require("./Routes/signUpRouter");
+const signInRouter = require("./Routes/logInRouter");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { user: req.user });
 });
 app.use("/sign-up", signUpRouter);
+
+app.use("/log-in", signInRouter);
+
+app.get("err", (err, req, res, next) => {
+  res.send(err);
+});
 
 app.listen(3000, () => {
   console.log("App listening on port http://localhost:3000");
