973891cbda6789109362ecb600904465624501f1
Damian Wujec <wujecdamian12@gmail.com>
2026-06-19 21:24:18 +0200

    feat(queries): add query for getting user by id or name

--- Models/queries.js
+++ Models/queries.js
@@ -5,6 +5,17 @@ const getAllUsers = async () => {
   return rows;
 };
 
+const getUserByUsername = async (username) => {
+  const { rows } = await db.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  return rows[0];
+};
+const getUserById = async (id) => {
+  const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+  return rows[0];
+};
+
 const createUser = async (fullName, username, password, passwordSalt) => {
   await db.query(
     "INSERT INTO users (full_name,username,password,password_salt) VALUES ($1,$2,$3,$4)",
@@ -14,5 +25,7 @@ const createUser = async (fullName, username, password, passwordSalt) => {
 
 module.exports = {
   getAllUsers,
+  getUserByUsername,
+  getUserById,
   createUser,
 };
