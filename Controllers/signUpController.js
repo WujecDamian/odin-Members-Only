1e14c18da5bbc47a560d9e3ff2b90692a0402171
Damian Wujec <wujecdamian12@gmail.com>
2026-06-19 21:23:20 +0200

    feat: add sign Up views & functionality

--- Controllers/signUpController.js
+++ Controllers/signUpController.js
@@ -4,13 +4,23 @@ const renderForm = (req, res) => {
   res.render("signUp");
 };
 
-const createUser = async (req, res) => {
-  const fullname = req.body.fullName;
-  const username = req.body.username;
-  const password = req.body.password;
-  const passwordConfirm = req.body.password;
-  const user = await db.createUser();
-  res.redirect("/");
+const createUser = async (req, res, next) => {
+  try {
+    const fullName = req.body.fullName;
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordConfirm = req.body.password;
+    const passwordSalt = "todo";
+    const user = await db.createUser(
+      fullName,
+      username,
+      password,
+      passwordSalt,
+    );
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = {
