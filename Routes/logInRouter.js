88a6306b26ab0b8e8494893f076e4e735ef91038
Damian Wujec <wujecdamian12@gmail.com>
2026-06-20 10:39:05 +0200

    feat: add validation and sanitization to log in form.

--- Routes/logInRouter.js
+++ Routes/logInRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const Router = express.Router();
 
+//controller
 const logInController = require("../Controllers/logInController");
+//validator
+const validateLogInForm = require("../validators/logInValidator");
 
 Router.get("/", logInController.renderForm);
-Router.post("/", logInController.logInUser);
+Router.post("/", validateLogInForm, logInController.logInUser);
 
 module.exports = Router;
---
+++ validators/logInValidator.js
@@ -0,0 +1,30 @@
+const { body, validationResult } = require("express-validator");
+
+const validateLogInForm = [
+  body("username")
+    .escape()
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .isLength({ min: 1, max: 40 })
+    .withMessage("Username must be between 1-40 characters")
+    .isAlpha()
+    .withMessage("Username cannot contain special characters"),
+  body("password")
+    .escape()
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isLength({ min: 8, max: 32 })
+    .withMessage("Password ust be between 8-32 characters")
+    .isAlpha()
+    .withMessage("Password cannot contain special characters"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = validateLogInForm;
